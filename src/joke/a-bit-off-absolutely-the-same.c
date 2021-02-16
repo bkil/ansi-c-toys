de57facdfa049d0693054221dd19b467f19d1836
bkil <b.K.il.h.u+tigbuh@gmail.com>
2021-02-16 22:47:22 +0100

    joke/a-bit-off-absolutely-the-same: add

---
+++ src/joke/a-bit-off-absolutely-the-same.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/*
+> - `2` says to `-2`: _"You look a bit off, why don't you stay as positive as I do?"_
+> - That one replies:  _"We're absolutely the same"_
+*/
+
+int main() {
+  printf("2: %x ; -2: %x\n", 2, -2);
+}
