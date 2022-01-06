bd2e2c27face00c9faa1082914cc620d1a6f1ad1
bkil <b.K.il.h.u+tigbuh@gmail.com>
2022-01-06 13:16:02 +0100

    src/common-netmask: `uint32_t` from stdint.h instead of `unsigned int`

--- src/common-netmask.c
+++ src/common-netmask.c
@@ -1,40 +1,40 @@
-#include <stdio.h>
-#include <stdbool.h>
+#include <stdio.h> // printf
+#include <stdbool.h> // bool
+#include <stdint.h> // uint32_t
 
-#define uint32 unsigned int
 #define BROADCAST 0xffffffff
 #define IP(a,b,c,d) ((a)<<24 | (b)<<16 | (c)<<8 | (d))
 
-uint32 get_lowest_one(uint32 x) {
+uint32_t get_lowest_one(uint32_t x) {
   return x & -x;
 }
 
-bool is_power_of_two(uint32 x) {
+bool is_power_of_two(uint32_t x) {
   return x == get_lowest_one(x);
 }
 
-uint32 clear_lowest_one(uint32 x) {
+uint32_t clear_lowest_one(uint32_t x) {
   return x ^ get_lowest_one(x);
 }
 
-uint32 keep_top_ones(uint32 x) {
+uint32_t keep_top_ones(uint32_t x) {
   while (!is_power_of_two(-x)) {
     x = clear_lowest_one(x);
   }
   return x;
 }
 
-uint32 equal_bits(uint32 a, uint32 b) {
+uint32_t equal_bits(uint32_t a, uint32_t b) {
   return a ^ ~b;
 }
 
-uint32 common_netmask_2(uint32 netmask, uint32 ip1, uint32 ip2) {
+uint32_t common_netmask_2(uint32_t netmask, uint32_t ip1, uint32_t ip2) {
   return keep_top_ones(netmask & equal_bits(ip1, ip2));
 }
 
-uint32 common_netmask(uint32 ip[]) {
-  uint32 netmask = BROADCAST;
-  uint32 base = ip[0];
+uint32_t common_netmask(uint32_t ip[]) {
+  uint32_t netmask = BROADCAST;
+  uint32_t base = ip[0];
   if (base == BROADCAST)
     return netmask;
 
@@ -45,13 +45,13 @@ uint32 common_netmask(uint32 ip[]) {
 }
 
 int main() {
-  uint32 ip[] = {
+  uint32_t ip[] = {
     IP(192,168,1,10),
     IP(192,168,2,10),
     IP(192,169,254,1),
     BROADCAST
   };
 
-  uint32 netmask = common_netmask(ip);
+  uint32_t netmask = common_netmask(ip);
   printf("%08x/%08x\n", ip[0] & netmask, netmask);
 }
