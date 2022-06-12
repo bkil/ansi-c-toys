d9f6d6b0043b073b461e2b7d2545207a3ab042ef
bkil <b.K.il.h.u+tigbuh@gmail.com>
2022-06-12 17:55:59 +0200

    src/io/ls-nondir.c: add

---
+++ src/io/ls-nondir.c
@@ -0,0 +1,37 @@
+#include <sys/types.h> // opendir closedir
+#include <dirent.h> // opendir readdir closedir
+#include <stdio.h> // puts perror
+#include <errno.h> // errno perror
+
+/*
+See `man readdir`. As `d_type` is not POSIX, this may not work on
+systems other than Linux and BSD and on certain file systems.
+*/
+
+int
+main(const int argc, const char**argv) {
+  errno = 0;
+  DIR *dirp = opendir(argc >= 2 ? argv[1] : ".");
+  if (!dirp) {
+    perror("opendir");
+    return 1;
+  }
+
+  errno = 0;
+  const struct dirent *entry;
+  while ((entry = readdir(dirp))) {
+    if (DT_REG == entry->d_type) {
+      puts(entry->d_name);
+    }
+    errno = 0;
+  }
+
+  if (errno) {
+    perror("readdir");
+    closedir(dirp);
+    return 1;
+  } else {
+    closedir(dirp);
+    return 0;
+  }
+}
